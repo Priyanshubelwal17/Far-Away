d26fcd24c350bda404cbf3241247082eea7982ed
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-05-14 21:39:00 +0530

    Implement derived state for stats and add conditional messages

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
         onDeleteItem={handleDeleteItems}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -43,7 +43,6 @@ function Form({ addItems }) {
   function handleSubmit(e) {
     e.preventDefault();
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     addItems(newItem);
 
@@ -56,7 +55,6 @@ function Form({ addItems }) {
       <select
         value={quantity}
         onChange={(e) => {
-          console.log(e.target.value);
           setQuantity(+e.target.value);
         }}
       >
@@ -71,7 +69,6 @@ function Form({ addItems }) {
         placeholder="Item..."
         value={description}
         onChange={(e) => {
-          console.log(e.target.value);
           setDescription(e.target.value);
         }}
       />
@@ -113,10 +110,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🛬</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em> 💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go 🛩️"
+          : `   💼 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)
+`}
+      </em>
     </footer>
   );
 }
