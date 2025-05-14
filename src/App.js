cf53cdc7870436a60bcdd0d1132c5783897576c7
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-05-14 16:24:04 +0530

    Learn Lifting state up

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form addItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,13 +26,16 @@ function Logo() {
   return <h1>🏝️ Far Away 🏇</h1>;
 }
 
-function Form() {
+function Form({ addItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    addItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -62,11 +70,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item, i) => (
+        {items.map((item, i) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
