5c2942d88ba71b93ba1c6f25aed9b77ae4bfe177
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-05-14 22:56:03 +0530

    Sort items

--- src/App.js
+++ src/App.js
@@ -78,10 +78,24 @@ function Form({ addItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("description");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.description));
   return (
     <div className="list">
       <ul>
-        {items.map((item, i) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -90,6 +104,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order </option>
+          <option value="description">Sort by description </option>
+          <option value="packed">Sort by packed status </option>
+        </select>
+      </div>
     </div>
   );
 }
